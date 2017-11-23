c030451e3f2a82a2e9339839c00dbb96c3e9c388
Rich Comber <rich@elementalwebdesign.co.uk>
2017-11-23 21:50:29 +0000

    Getting basic server side fetch hooked up

--- source/js/routes/home/index.js
+++ source/js/routes/home/index.js
@@ -5,17 +5,35 @@ import { translate } from 'react-i18next';
 
 // HOC
 import Page from '../../hoc/page';
+import { testFetch } from '../../actions/test';
 
 // COMPONENTS
 
 class Home extends Component {
+  static getInitialProps(store) {
+    return store.dispatch(testFetch());
+  }
+  
   componentDidMount() {
-    this.props.testFetch();
+    if (!this.loaded()) {
+      this.props.testFetch();
+    }
+  }
+
+  loaded() {
+    const { test } = this.props;
+    return !test.fetching && !test.error && Object.keys(test.data).length;
   }
 
   renderContent() {
     const { test } = this.props;
-    return null;
+    if (this.loaded()) {
+      return ([
+        <h1 key='title'>{ test.data.title }</h1>,
+        <p key='body'>{ test.data.body }</p>
+      ]);
+    }
+    return <p>Fetching</p>;
   }
   
   render() {
@@ -33,7 +51,9 @@ class Home extends Component {
 }
 
 Home.propTypes = {
-  t: PropTypes.func.isRequired
+  t: PropTypes.func.isRequired,
+  testFetch: PropTypes.func.isRequired,
+  test: PropTypes.object.isRequired
 };
 
 export default translate(['common'])(Page(Home));
